fd03c7ef5af87e4380b58b47f7bb7b895cbec102
savadam <savad1989@gmail.com>
2022-06-23 01:10:43 +0400

    testing token

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import telebot
 
-TOKEN = os.environ.get('API_TOKEN', None)
+# TOKEN = os.environ.get('API_TOKEN', None)
+TOKEN = '5513863043:AAG5b6vY-GcTNrXyfFvGf_notlv4js2wjds'
 
 telegram_bot = telebot.TeleBot(TOKEN)
 telebot_chat_id = '471928485'
