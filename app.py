7044ac35a878a802b18d844f41a521842cd98917
savadam <savad1989@gmail.com>
2022-06-23 01:38:50 +0400

    testing flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import os
+from os import environ
 import telebot
+from flask import Flask
 
 # TOKEN = os.environ.get('API_TOKEN', None)
 TOKEN = '5513863043:AAG5b6vY-GcTNrXyfFvGf_notlv4js2wjds'
@@ -8,3 +9,6 @@ telegram_bot = telebot.TeleBot(TOKEN)
 telebot_chat_id = '471928485'
 
 telegram_bot.send_message(telebot_chat_id, 'Hello')
+
+app = Flask(__name__)
+app.run(environ.get('PORT'))
