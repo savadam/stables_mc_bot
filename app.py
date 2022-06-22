6e2cefe8ab168491d5cc452069a35d515de8cbcb
savadam <savad1989@gmail.com>
2022-06-23 01:44:41 +0400

    token placed

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from os import environ
+import os
 import telebot
 from flask import Flask
 
-# TOKEN = os.environ.get('API_TOKEN', None)
-TOKEN = '5513863043:AAG5b6vY-GcTNrXyfFvGf_notlv4js2wjds'
+TOKEN = os.environ.get('API_TOKEN', None)
 
 telegram_bot = telebot.TeleBot(TOKEN)
 telebot_chat_id = '471928485'
@@ -11,4 +10,4 @@ telebot_chat_id = '471928485'
 telegram_bot.send_message(telebot_chat_id, 'Hello')
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+app.run(os.environ.get('PORT'))
